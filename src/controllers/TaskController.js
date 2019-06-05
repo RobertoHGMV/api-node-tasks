cbeddf5679a412c8c014b5327b2c657d94a0ab2f
Roberto Gomes <robertohgm@hotmail.com>
2019-06-04 23:30:12 -0300

    Adição de validação

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -1,39 +1,86 @@
 const mongoose = require('mongoose');
 
 const Task = mongoose.model('Task');
+const STATUS_CREATED = 201;
+const STATUS_NOCONTENT = 204;
+const STATUS_BADREQUEST = 400;
+
+const Errors = [];
+
+validate = (task) => {
+    if (!task.title || task.title === "")
+        Errors.push('Título não informado');
+
+    if (!task.description || task.description === "")
+        Errors.push('Descrição não informada');
+
+    if (Errors.length > 0)
+        throw new Error();
+}
 
 module.exports = {
     async getAllPaginate(req, res) {
-        const { page = 1 } = req.query;
-        const tasks = await Task.paginate({}, { page, limit: 10 });
-        return res.json(tasks);
+        try {
+            const { page = 1 } = req.query;
+            const tasks = await Task.paginate({}, { page, limit: 10 });
+            return res.json(tasks);
+        }
+        catch (ex) {
+            return res.status(STATUS_BADREQUEST).send(Errors);
+        }
     },
 
     async getAll(req, res) {
-        const tasks = await Task.find();
-        return res.json(tasks);
+        try {
+            const tasks = await Task.find();
+            return res.json(tasks);
+        }
+        catch (ex) {
+            return res.status(STATUS_BADREQUEST).send(Errors);
+        }
     },
 
     async getByKey(req, res) {
-        const task = await Task.findById(req.params.id);
-        return res.json(task);
+        try {
+            const task = await Task.findById(req.params.id);
+            return res.json(task);
+        }
+        catch (ex) {
+            return res.status(STATUS_BADREQUEST).send(Errors);
+        }
     },
 
     async create(req, res) {
-        console.log(req.body);
-        const task = await Task.create(req.body);
-        return res.json(task);
+        try {
+            validate(req.body);
+
+            const task = await Task.create(req.body);
+            return res.status(STATUS_CREATED).json(task);
+        }
+        catch (ex) {
+            return res.status(STATUS_BADREQUEST).send(Errors);
+        }
     },
 
     async update(req, res) {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        return res.json(task);
+        try {
+            validate(req.body);
+
+            const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+            return res.json(task);
+        }
+        catch (ex) {
+            return res.status(STATUS_BADREQUEST).send(Errors);
+        }
     },
 
     async delete(req, res) {
-        await Task.findByIdAndRemove(req.params.id);
-        return res.send();
+        try {
+            await Task.findByIdAndRemove(req.params.id);
+            return res.status(STATUS_NOCONTENT).send();
+        }
+        catch (ex) {
+            return res.status(STATUS_BADREQUEST).send(ex);
+        }
     }
-
-    //res.status(400).send('Current password does not match');
 }
